b72d39906dd22d66910c7e71ac0b870ba4d74839
iramaro <iramaro@disroot.org>
2021-06-22 01:11:36 -0300

    WIP, model tests (simple only).

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is invalid if the email and password are empty" do
+    expect(User.create(email: '', password: '')).to_not be_valid
+  end
+  it "is valid if the email and password are filled" do
+    expect(User.create(email: 'test@mokbeats.com', password: '123456')).to be_valid
+  end
 end
